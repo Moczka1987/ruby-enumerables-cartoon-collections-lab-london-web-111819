dcc9752104a85fea9a0428005d56edd276924ce9
Mariola Pasko <paskomariola@gmail.com>
2019-11-04 12:37:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,17 +3,3 @@ array.each_with_index do |char, index|
 puts "#{index + 1} " "#{char}" 
 end 
 end
-
-
-def summon_captain_planet(array)
-array.map {|word| "#{word}!".capitalize}
-end
-end
-
-def long_planeteer_calls(array)
-array.any? {|word| word.length > 4}
-end
-
-def find_the_cheese(array)
-    array.include?("cheddar" || "gouda" || "camembert")
-end
