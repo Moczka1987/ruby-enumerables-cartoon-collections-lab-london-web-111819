bb26b3e89fa5369dcefd4ce65a2c3f0542e77f7c
Mariola Pasko <paskomariola@gmail.com>
2019-11-04 12:38:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,3 +3,11 @@ def roll_call_dwarves(dwarves)
     puts "#{index + 1}. #{dwarf}"
   end
 end	
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
+end	
+end
+
+
+
