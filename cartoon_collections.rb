b09911563a0e533ec05490d7f80fa0c340da5f15
Mariola Pasko <paskomariola@gmail.com>
2019-11-04 12:40:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,29 +1,19 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, index|
-    puts "#{index + 1}. #{dwarf}"
-  end
-end	
-
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| call.capitalize + '!' }
-end	
+def roll_call_dwarves(array)
+array.each_with_index do |char, index| 
+puts "#{index + 1} " "#{char}" 
+end 
 end
 
-def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| call.length > 4 }
-end	
+
+def summon_captain_planet(array)
+array.map {|word| "#{word}!".capitalize}
+end
 end
 
-potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
+def long_planeteer_calls(array)
+array.any? {|word| word.length > 4}
+end
 
-def find_the_cheese(potentially_cheesy_items)
-  cheeses = %w[gouda cheddar camembert]
-
-  potentially_cheesy_items.find do |maybe_cheese|
-    cheeses.include?(maybe_cheese)
-  end
-end	end
-
-find_the_cheese(potentially_cheesy_items)
-
-
+def find_the_cheese(array)
+    array.include?("cheddar" || "gouda" || "camembert")
+end
